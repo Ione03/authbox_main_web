02692d22e54eda126e0e3db664f031fba357a99b
ione03 <suratiwan03@gmail.com>
2026-04-28 20:45:16 +0800

    Add sidemenu on dashboard

---
+++ src/routes/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+import { component$, Slot } from "@builder.io/qwik";
+import type { RequestHandler } from "@builder.io/qwik-city";
+import { ThemeProvider } from "../../context/theme-context";
+
+export const onGet: RequestHandler = async ({ cacheControl }) => {
+    cacheControl({
+        staleWhileRevalidate: 60 * 60 * 24 * 7,
+        maxAge: 5,
+    });
+};
+
+export default component$(() => {
+    return (
+        <ThemeProvider>
+            <Slot />
+        </ThemeProvider>
+    );
+});
